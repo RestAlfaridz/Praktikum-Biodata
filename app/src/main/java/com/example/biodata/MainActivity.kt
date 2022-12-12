cc5baf93dc41ed2e5640df2ea3a59d228b30df09
RestAlfaridz <62842765+RestAlfaridz@users.noreply.github.com>
2022-12-12 20:00:49 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/biodata/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.barvolume
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.TextView
+import com.example.biodata.R
+import org.w3c.dom.Text
+import java.util.*
+
+class MainActivity_biodata : AppCompatActivity(), View.OnClickListener {
+    lateinit var etTanggal : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnSimpan: Button = findViewById(R.id.btn_simpan)
+        btnSimpan.setOnClickListener(this)
+
+        etTanggal = findViewById(R.id.et_input_tanggal)
+        etTanggal.setOnClickListener {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this, { view, year, monthOfYear, dayOfMonth ->
+                val dat = (dayOfMonth.toString() + "-" +
+                        (monthOfYear + 1) + "-" + year)
+                etTanggal.setText(dat)
+            }, year, month, day)
+            datePickerDialog.show()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_simpan -> {
+                val etNamaDepan: EditText = findViewById(R.id.et_input_namaDepan)
+                val etNamaBelakang: EditText = findViewById(R.id.et_input_namaBelakang)
+                val etTanggal: EditText = findViewById(R.id.et_input_tanggal)
+                val etAlamat: EditText = findViewById(R.id.et_input_alamat)
+                val etNomor: EditText = findViewById(R.id.et_input_nomor)
+
+                val tvNama: TextView = findViewById(R.id.tv_output_nama)
+                val tvTanggal: TextView = findViewById(R.id.tv_output_tanggal)
+                val tvAlamat: TextView = findViewById(R.id.tv_output_alamat)
+                val tvNomor: TextView = findViewById(R.id.tv_output_nomor)
+
+                val textNama = "Nama : ${etNamaDepan.getText().toString()}"
+                val textTanggal = "Tangal Lahir : ${etTanggal.getText().toString()}"
+                val textAlamat = "Alamat : ${etAlamat.getText().toString()}"
+                val textNomor = "Nomor Telp : ${etNomor.getText().toString()}"
+
+                tvNama.text = textNama
+                tvTanggal.text = textTanggal
+                tvAlamat.text = textAlamat
+                tvNomor.text = textNomor
+            }
+        }
+    }
+}
